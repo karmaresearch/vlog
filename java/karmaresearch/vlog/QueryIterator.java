94d9cb8d01f5f88b2aa46d0889636256e9fecf2b
Jacopo Urbani <jacopo@cs.vu.nl>
2018-02-28 10:20:29 +0100

    added some java methods

---
+++ java/karmaresearch/vlog/QueryIterator.java
@@ -0,0 +1,4 @@
+package karmaresearch.vlog;
+
+class QueryIterator {
+};
--- java/karmaresearch/vlog/VLog.java
+++ java/karmaresearch/vlog/VLog.java
@@ -6,7 +6,17 @@ public class VLog {
         System.loadLibrary("vlog");
     }
 
+    //This method starts the reasoner. As input, we pass the content (as a string)
+    //of the edb configuration
     public native void start(String edbconfig);
+
+    //This method is used to query all facts with a simple predicate
+    public native QueryIterator query(int predID, Term[] terms);
+
+    //This method stops and clear the reasoner. If this method is not called,
+    //then there will be a memory leak since the reasoner will not be deallocated
+
+    public native void stop();
 
     public static void main(String[] args) {
         VLog vlog = new VLog();
