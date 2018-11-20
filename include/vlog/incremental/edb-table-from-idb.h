f6b7dcd64a8487cc31d9fa9f39370c30542391d5
Rutger Hofman <rutger@cs.vu.nl>
2018-11-20 18:24:06 +0100

    Begin typing the most trivial case of EDBonIDB cache

--- include/vlog/incremental/edb-table-from-idb.h
+++ include/vlog/incremental/edb-table-from-idb.h
@@ -9,212 +9,7 @@
 
 #include <vlog/fcinttable.h>
 #include <vlog/seminaiver.h>
-#include <vlog/inmemory/inmemorytable.h>
 
-class EDBonIDBIterator : public EDBIterator {
-protected:
-    const Literal &query;
-    const std::shared_ptr<SemiNaiver> prevSemiNaiver;
-    const PredId_t predid;
-    FCIterator idbItr;
-    std::shared_ptr<const FCInternalTable> idbInternalTable;
-    FCInternalTableItr *idbInternalItr;
-
-public:
-    EDBonIDBIterator(const Literal &query, const std::shared_ptr<SemiNaiver> SN) :
-            query(query), prevSemiNaiver(SN),
-            predid(query.getPredicate().getId()), idbInternalItr(NULL) {
-        init();
-    }
-
-    virtual void init() {
-        idbItr = prevSemiNaiver->getTable(query, 0,
-                                          prevSemiNaiver->getCurrentIteration());
-        if (! idbItr.isEmpty()) {
-            idbInternalTable = idbItr.getCurrentTable();
-            idbInternalItr = idbInternalTable->getIterator();
-        }
-    }
-
-    virtual bool hasNext() {
-        LOG(DEBUGL) << "         hasNext() on " << query.tostring();
-        if (idbInternalItr == NULL) {
-            return false;
-        }
-        if (idbInternalItr->hasNext()) {
-            return true;
-        }
-
-        idbInternalTable->releaseIterator(idbInternalItr);
-        idbItr.moveNextCount();
-        if (idbItr.isEmpty()) {
-            idbInternalItr = NULL;
-            return false;
-        }
-
-        idbInternalTable = idbItr.getCurrentTable();
-        idbInternalItr = idbInternalTable->getIterator();
-
-        return idbInternalItr->hasNext();
-    }
-
-    virtual void next() {
-        LOG(DEBUGL) << "         next() on " << query.tostring();
-        if (! idbInternalItr->hasNext()) {
-            LOG(ERRORL) << "next() on a table that hasNext() = false";
-            throw 10;               // follow convention
-        }
-        return idbInternalItr->next();
-    }
-
-    virtual Term_t getElementAt(const uint8_t p) {
-        LOG(DEBUGL) << "get element[" << (int)p << "] of " << query.tostring() << " = " << idbInternalItr->getCurrentValue(p);
-        return idbInternalItr->getCurrentValue(p);
-    }
-
-    virtual PredId_t getPredicateID() {
-        return predid;
-    }
-
-    virtual void moveTo(const uint8_t field, const Term_t t) {
-        throw 10;
-    }
-
-    virtual void skipDuplicatedFirstColumn() {
-        LOG(ERRORL) << "FIXME: implement " << typeid(*this).name() << "::" <<
-            __func__;
-    }
-
-    virtual void clear() {
-        LOG(ERRORL) << "FIXME: implement " << typeid(*this).name() << "::" <<
-            __func__;
-    }
-
-    virtual const char *getUnderlyingArray(uint8_t column) {
-        return NULL;
-    }
-
-    /*
-    No need to override
-    virtual std::pair<uint8_t, std::pair<uint8_t, uint8_t>> getSizeElemUnderlyingArray(uint8_t column) {
-        return std::make_pair(0, make_pair(0, 0));
-    }
-    */
-
-    virtual ~EDBonIDBIterator() {}
-};
-
-class EDBonIDBSortedIterator : public EDBIterator {
-protected:
-    const Literal &query;
-    const std::vector<uint8_t> &fields;
-    const std::shared_ptr<SemiNaiver> prevSemiNaiver;
-    EDBLayer *layer;
-    const PredId_t predid;
-    InmemoryTable *inmemoryTable;
-    EDBIterator *itr;
-
-public:
-    EDBonIDBSortedIterator(const Literal &query,
-                           const std::vector<uint8_t> &fields,
-                           const std::shared_ptr<SemiNaiver> SN,
-                           EDBLayer *layer) :
-            query(query), fields(fields), prevSemiNaiver(SN), layer(layer),
-            predid(query.getPredicate().getId()) {
-        LOG(DEBUGL) << "EDBonIDBSortedIterator, query " << query.tostring() <<
-            ", fields.size() " << fields.size();
-        LOG(INFOL) << "FIXME: if fields implies a no-op, dispatch to non-sorted Iterator";
-        EDBonIDBIterator between(query, prevSemiNaiver);
-        // Collect matching data. Will be stored in an InmemoryTable.
-        std::vector<std::vector<Term_t>> rows;
-        std::vector<Term_t> term(query.getTupleSize());
-        // need to store all variables, then afterwards sort by fields
-        std::vector<bool> isVariable(query.getTupleSize());
-        VTuple tuple = query.getTuple();
-        for (size_t i = 0; i < query.getTupleSize(); ++i) {
-            VTerm vt = tuple.get(i);
-            isVariable[i] = vt.isVariable();
-            if (! isVariable[i]) {
-                // fill out the constants
-                term[i] = vt.getValue();
-            }
-        }
-        while (between.hasNext()) {
-            between.next();
-            size_t nVars = 0;
-            for (size_t i = 0; i < query.getTupleSize(); ++i) {
-                if (isVariable[i]) {
-                    term[i] = between.getElementAt(nVars);
-                    ++nVars;
-                }
-            }
-            rows.push_back(term);
-        }
-
-        if (rows.size() == 0) {
-            inmemoryTable = NULL;
-        } else {
-            inmemoryTable = new InmemoryTable(predid, rows, layer);
-            itr = inmemoryTable->getSortedIterator(query, fields);
-        }
-    }
-
-    virtual bool hasNext() {
-        LOG(DEBUGL) << "         hasNext() on " << query.tostring();
-        if (inmemoryTable == NULL) {
-            return false;
-        }
-
-        return itr->hasNext();
-    }
-
-    virtual void next() {
-        LOG(DEBUGL) << "         next() on " << query.tostring();
-        return itr->next();
-    }
-
-    virtual Term_t getElementAt(const uint8_t p) {
-        LOG(DEBUGL) << "get element[" << (int)p << "] of " << query.tostring() << " = " << itr->getElementAt(p);
-        return itr->getElementAt(p);
-    }
-
-    virtual PredId_t getPredicateID() {
-        return predid;
-    }
-
-    virtual void moveTo(const uint8_t field, const Term_t t) {
-        throw 10;
-    }
-
-    virtual void skipDuplicatedFirstColumn() {
-        LOG(ERRORL) << "FIXME: implement " << typeid(*this).name() << "::" <<
-            __func__;
-    }
-
-    virtual void clear() {
-        LOG(ERRORL) << "FIXME: implement " << typeid(*this).name() << "::" <<
-            __func__;
-    }
-
-    virtual const char *getUnderlyingArray(uint8_t column) {
-        return NULL;
-    }
-
-    /*
-    No need to override
-    virtual std::pair<uint8_t, std::pair<uint8_t, uint8_t>> getSizeElemUnderlyingArray(uint8_t column) {
-        return std::make_pair(0, make_pair(0, 0));
-    }
-    */
-
-    virtual ~EDBonIDBSortedIterator() {
-        LOG(ERRORL) << "Need to cache inmemoryTable";
-        if (inmemoryTable != NULL) {
-            inmemoryTable->releaseIterator(itr);
-            delete inmemoryTable;
-        }
-    }
-};
 
 /**
  * Transfer an IDB predicate of the previous SemiNaiver to be
@@ -225,18 +20,18 @@ private:
     PredId_t    predid;
     const std::shared_ptr<SemiNaiver> prevSemiNaiver;
     const FCTable *idbTable;
+    EDBTable *naturalTable;
     size_t cardinality;
+
+    static bool isNatural(const Literal &query,
+                          const std::vector<uint8_t> fields);
 
 public:
     EDBLayer *layer;
     EDBonIDBTable(PredId_t predid, EDBLayer *layer,
-                  const std::shared_ptr<SemiNaiver> prevSN) :
-            predid(predid), layer(layer), prevSemiNaiver(prevSN),
-            idbTable(prevSN->getTable(predid, prevSN->getCurrentIteration())) {
-        cardinality = prevSN->getProgram()->getPredicateCard(predid);
-        LOG(DEBUGL) << "prevSN iteration " << prevSN->getCurrentIteration() <<
-            " card. " << cardinality << " size " << idbTable->getNAllRows();
-    }
+                  const std::shared_ptr<SemiNaiver> prevSN);
+
+    virtual ~EDBonIDBTable();
 
     //execute the query on the knowledge base
     virtual void query(QSQQuery *query, TupleTable *outputTable,
@@ -267,22 +62,12 @@ public:
        throw 10;
     }
 
-    virtual EDBIterator *getIterator(const Literal &q) {
-        LOG(DEBUGL) << "Get iterator for query " << q.tostring(NULL, layer);
-        return new EDBonIDBIterator(q, prevSemiNaiver);
-    }
+    virtual EDBIterator *getIterator(const Literal &q);
 
     virtual EDBIterator *getSortedIterator(const Literal &query,
-                                           const std::vector<uint8_t> &fields) {
-        LOG(INFOL) << "Get SortedIterator for query " <<
-            query.tostring(NULL, layer) << " fields " << fields.size();
-        LOG(INFOL) << "FIXME: implement cache for sorted query";
-        return new EDBonIDBSortedIterator(query, fields, prevSemiNaiver, layer);
-    }
+                                           const std::vector<uint8_t> &fields);
 
-    virtual void releaseIterator(EDBIterator *itr) {
-        delete itr;
-    }
+    virtual void releaseIterator(EDBIterator *itr);
 
     virtual bool getDictNumber(const char *text, const size_t sizeText,
                                uint64_t &id) {
@@ -313,37 +98,7 @@ public:
         return predid;
     }
 
-    std::ostream &dump(std::ostream &os) {
-        std::string name = layer->getPredName(predid);
-        os << (void *)this << " Table " << name << std::endl;
-        const uint8_t sizeRow = getArity();
-        Predicate pred(predid, 0, EDB, sizeRow);
-        VTuple t = VTuple(sizeRow);
-        for (uint8_t i = 0; i < t.getSize(); ++i) {
-            t.set(VTerm(i + 1, 0), i);
-        }
-        Literal lit(pred, t);
-        os << "Literal@" << int(sizeRow) << " " << lit.tostring() << std::endl;
-        EDBIterator *itr = getIterator(lit);
-        while (itr->hasNext()) {
-            itr->next();
-            for (uint8_t m = 0; m < sizeRow; ++m) {
-                os << "\t";
-                uint64_t v = itr->getElementAt(m);
-                std::string buffer;
-                if (getDictText(v, buffer)) {
-                    os << buffer;
-                } else {
-                    os << to_string(v);
-                }
-            }
-            os << std::endl;
-        }
-
-        releaseIterator(itr);
-
-        return os;
-    }
+    std::ostream &dump(std::ostream &os);
 };
 
 /*
