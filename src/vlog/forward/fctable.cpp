39e5ec7bd213d31221b056f3ae58719a08a5d84c
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2019-04-04 09:46:56 +0200

    bug fix

--- src/vlog/forward/fctable.cpp
+++ src/vlog/forward/fctable.cpp
@@ -99,7 +99,7 @@ std::shared_ptr<const FCTable> FCTable::filter(const Literal &literal,
 
     if (shouldFilter) {
         if (blocks.size() == 0) {
-            return std::shared_ptr<FCTable>(this);
+            return std::shared_ptr<FCTable>(new FCTable(NULL, sizeRow));
         }
         std::shared_ptr<FCTable> output;
         std::vector<FCBlock>::iterator itr = blocks.begin();
