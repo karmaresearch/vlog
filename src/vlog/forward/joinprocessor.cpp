60ce0fe38a50639e078ddafa77845e52a7eb04d6
Larry Gonzalez <larryjgonzalez@gmail.com>
2019-09-06 17:51:00 +0200

    clear for loop

--- src/vlog/forward/joinprocessor.cpp
+++ src/vlog/forward/joinprocessor.cpp
@@ -813,12 +813,10 @@ void JoinExecutor::execSelectiveHashJoin(const RuleExecutionDetails & currentRul
                     //Filter the tables in input checking whether the input query produced
                     //by the rule can have produced output tuples in following derivations
                     uint32_t nEmptyDerivations = 0;
-                    for (std::vector<std::pair<const FCBlock *, uint32_t>>::iterator itr = tables.begin();
-                            itr != tables.end(); ++itr) {
-                        if (queryFilterer.
-                                producedDerivationInPreviousSteps(l,
-                                    literalToQuery, itr->first)) {
-                            itr->second--;
+                    for (auto& table : tables) {
+                        if (queryFilterer.producedDerivationInPreviousSteps(
+                                    l, literalToQuery, table.first)) {
+                            table.second--;
                             nEmptyDerivations++;
                         }
                     }
@@ -843,12 +841,11 @@ void JoinExecutor::execSelectiveHashJoin(const RuleExecutionDetails & currentRul
 
                 {
                     std::vector<FilterHashJoinBlock> retainedTables;
-                    for (std::vector<std::pair<const FCBlock *, uint32_t>>::iterator itr = tables.begin();
-                            itr != tables.end(); ++itr) {
-                        if (itr->second > 0) {
+                    for (auto& table : tables) {
+                        if (table.second > 0) {
                             FilterHashJoinBlock b;
-                            b.table = itr->first->table.get();
-                            b.iteration = itr->first->iteration;
+                            b.table = table.first->table.get();
+                            b.iteration = table.first->iteration;
                             retainedTables.push_back(b);
                         }
                     }
@@ -887,10 +884,8 @@ void JoinExecutor::execSelectiveHashJoin(const RuleExecutionDetails & currentRul
                     //std::chrono::duration<double> secJ = std::chrono::system_clock::now() - startJ;
                 } //retained tables
 
-                for (std::vector<DuplicateContainers>::iterator itr = existingTuples.begin();
-                        itr != existingTuples.end();
-                        ++itr) {
-                    itr->clear();
+                for (auto& tuple : existingTuples) {
+                    tuple.clear();
                 }
 
                 //size_t uniqueDerivation = out->getUniqueDerivation();
