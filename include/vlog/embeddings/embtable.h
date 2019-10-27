3550264fd4fc507431d684f9c24263f2dc0f7381
Jacopo Urbani <jacopo@cs.vu.nl>
2019-10-28 10:34:51 +1300

    adding stub for the ML table

---
+++ include/vlog/embeddings/embtable.h
@@ -0,0 +1,72 @@
+#ifndef _EMB_LAYER_H
+#define _EMB_LAYER_H
+
+#include <vlog/column.h>
+#include <vlog/edbtable.h>
+#include <vlog/edbiterator.h>
+#include <vlog/segment.h>
+
+#include <trident/ml/embeddings.h>
+
+class EmbTable : public EDBTable {
+    private:
+        const PredId_t predid;
+        EDBLayer *layer;
+        const std::string folder;
+        const bool entityOrRel;
+        std::shared_ptr<Embeddings<double>> emb;
+
+    public:
+        virtual uint8_t getArity() const {
+            return 2;
+        }
+
+        bool areTermsEncoded() {
+            return true;
+        }
+
+        EmbTable(PredId_t predid,
+                EDBLayer *layer,
+                std::string folder,
+                bool entityOrRel = true);
+
+        void query(QSQQuery *query, TupleTable *outputTable,
+                std::vector<uint8_t> *posToFilter,
+                std::vector<Term_t> *valuesToFilter);
+
+        size_t estimateCardinality(const Literal &query);
+
+        size_t getCardinality(const Literal &query);
+
+        size_t getCardinalityColumn(const Literal &query, uint8_t posColumn);
+
+        bool isEmpty(const Literal &query, std::vector<uint8_t> *posToFilter,
+                std::vector<Term_t> *valuesToFilter);
+
+        EDBIterator *getIterator(const Literal &query);
+
+        EDBIterator *getSortedIterator(const Literal &query,
+                const std::vector<uint8_t> &fields);
+
+        bool getDictNumber(const char *text, const size_t sizeText,
+                uint64_t &id);
+
+        bool getDictText(const uint64_t id, char *text);
+
+        bool getDictText(const uint64_t id, std::string &text);
+
+        bool expensiveLayer() {
+            return true;
+        }
+
+        uint64_t getNTerms();
+
+        void releaseIterator(EDBIterator *itr);
+
+        uint64_t getSize();
+
+        ~EmbTable();
+};
+
+
+#endif
---
+++ src/vlog/embeddings/embtable.cpp
@@ -0,0 +1,87 @@
+#include <vlog/embeddings/embtable.h>
+
+
+EmbTable::EmbTable(PredId_t predid,
+        EDBLayer *layer,
+        std::string folder,
+        bool entityOrRel) : predid(predid), layer(layer), folder(folder),
+    entityOrRel(entityOrRel) {
+        if (entityOrRel) {
+            emb = Embeddings<double>::load(folder + "/E");
+        } else {
+            emb = Embeddings<double>::load(folder + "/R");
+        }
+    }
+
+void EmbTable::query(QSQQuery *query, TupleTable *outputTable,
+        std::vector<uint8_t> *posToFilter,
+        std::vector<Term_t> *valuesToFilter) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+size_t EmbTable::estimateCardinality(const Literal &query) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+size_t EmbTable::getCardinality(const Literal &query) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+size_t EmbTable::getCardinalityColumn(const Literal &query, uint8_t posColumn) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+bool EmbTable::isEmpty(const Literal &query, std::vector<uint8_t> *posToFilter,
+        std::vector<Term_t> *valuesToFilter) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+EDBIterator *EmbTable::getIterator(const Literal &query) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+EDBIterator *EmbTable::getSortedIterator(const Literal &query,
+        const std::vector<uint8_t> &fields) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+bool EmbTable::getDictNumber(const char *text, const size_t sizeText,
+        uint64_t &id) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+bool EmbTable::getDictText(const uint64_t id, char *text) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+bool EmbTable::getDictText(const uint64_t id, std::string &text) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+uint64_t EmbTable::getNTerms() {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+void EmbTable::releaseIterator(EDBIterator *itr) {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+uint64_t EmbTable::getSize() {
+    LOG(ERRORL) << "EmbTable: Not supported";
+    throw 10;
+}
+
+EmbTable::~EmbTable() {
+}
